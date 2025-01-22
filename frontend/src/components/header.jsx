941c440e8f27c8fc8428551ed79aa47d608eb2a1
KANZO-TUN <will2002@outlook.jp>
2025-01-22 20:47:55 +0900

    first commit

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,24 @@
+import './Header.css';
+
 const Header = () => {
-    return (
-        <div>Yeah!</div>
-    )
-}
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          {/* うさぎのロゴ */}
+          <img src="../assets/rabbit-logo.png" alt="ロゴ" /> 
+        </div>
+        <nav className="nav">
+          <ul>
+            <li><a href="/">ホーム</a></li>
+            <li><a href="/about">私たちについて</a></li>
+            <li><a href="/service">サービス</a></li>
+            <li><a href="/contact">お問い合わせ</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
